1d42cf119eaef43a0d4c8e99a804edf6038ecd0d
Noah Wilson <ntw9049@g.rit.edu>
2016-10-11 16:37:00 -0400

    Recommit

--- Blocks Game/Blocks/LoadFile.cs
+++ Blocks Game/Blocks/LoadFile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;5
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Audio;
 
 namespace TileGame
